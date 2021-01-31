71dfe4e8168012faa44676e0ef647bb3db64a62a
LeoIta <leonardolorusso83@gmail.com>
2021-01-31 22:34:38 +0100

    start project

---
+++ Weather-project/core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.CharField(max_length=50,null=True,blank=True )
+    temperature = models.IntegerField(null=True,blank=True)
+    feeling = models.IntegerField(null=True,blank=True)
+    wind = models.IntegerField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): #show the actual city name
+        return self.name
+
+    class Meta: #show the plural of city as cities instead of citys
+        verbose_name_plural = 'cities'
